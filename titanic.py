5741869562e331749a3d8a405c31baa44ec2a08e
Benjamin Otoo <116750841+BenjaminOtoo@users.noreply.github.com>
2023-03-11 13:17:37 +0000

    add data exploration and preparation code

--- titanic.py
+++ titanic.py
@@ -4,3 +4,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
+#Explore dataset
+titanic_df = pd.read_csv('train.csv')
+print(titanic_df.head())
+print(titanic_df.info())
+
+#Prepare the data for training
+titanic_df = titanic_df.drop(['PassengerId', 'Name', 'Ticket', 'Cabin', 'Embarked'], axis=1)
+titanic_df['Age'] = titanic_df['Age'].fillna(titanic_df['Age'].median())
+titanic_df['Sex'] = titanic_df['Sex'].map({'male': 0, 'female': 1})
+
+X = titanic_df.drop('Survived', axis=1)
+y = titanic_df['Survived']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
